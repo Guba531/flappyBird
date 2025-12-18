ef0e0c4de2f7d20057d90c4d25315418a6100dfc
Guba <gustavopessoa211@gmail.com>
2025-12-17 16:45:41 -0800

    Hoje eu fiz boa parte do meu js como um loop, objetos, funções e mais.

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-const gameBoard = document.getElementById('gameboard');
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+const restartBtn = document.getElementById('resetBtn');
+
+let jogoAtivo = false;
+
+const img = {
+    bird: new Image(), 
+    pipe: new Image(),
+    bg: new Image()
+}
+
+img.bird.src = "img/bird.png";
+img.pipe.src = "img/cano.jpg";
+img.bg.src = "img/fundo.png";
+
+let fundoX = 0;
+const speed = 1;
+
+function bgLoop() {
+    if (img.bg.complete) {
+        ctx.drawImage(img.bg, fundoX, 0, canvas.width, canvas.height);
+        ctx.drawImage(img.bg, fundoX + canvas.width, 0, canvas.width, canvas.height);
+        if (jogoAtivo) {
+            fundoX -= speed;
+            if (fundoX <= -canvas.width) {
+                fundoX = 0;
+            }
+        }
+    } else {
+        ctx.fillStyle = "light-blue";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+function startGame() {
+    gameStarted = true;
+    startTime = Date.now();
+
+    timerInterval = setInterval(updateTimer, 1000);
+}
