6e4bdd8b950b04d32a0410850b15b0996079d06f
Guba <gustavopessoa211@gmail.com>
2026-01-14 16:31:47 -0800

    Hoje eu aprendi sobre o local storage e tambem aprendi o quão importante e para saber de operações matematicas no js.

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 const restartBtn = document.getElementById('resetBtn');
 const startBtn = document.getElementById('startBtn');
+const score = document.getElementById('scores');
+const records = document.getElementById('records');
 
 let jogoAtivo = false;
+let points = 0;
+let record = localStorage.getItem('records')||0;
+records.textContent=record;
 
 const img = {
     bird: new Image(), 
@@ -27,6 +32,23 @@ Object.values(img).forEach(img => {
     }
 });
 
+const bird={
+    width: 50,
+    height: 50,
+    jump: -3,
+    fall: 0.5,
+    birdSpeed: 0,
+    rotate: 0,
+    x: 50,
+    y: canvas.height / 2,
+}
+
+let pipe=[];
+const pipeDistance = 250;
+const pipeHeight = 20;
+const pipeHeightDistance = 100;
+let frameCount = 0;
+
 function bgLoop() {
     if (img.bg.complete) {
         ctx.drawImage(img.bg, fundoX, 0, canvas.width, canvas.height);
@@ -41,6 +63,31 @@ function bgLoop() {
         ctx.fillStyle = "light-blue";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
+}
+
+function drawBird() {
+    ctx.save();
+
+    bird.rotate = Math.min(Math.max(bird.speed * 3, -25), 90);
+
+    ctx.translate(bird.x + bird.width/2, bird.y + bird.height/2);
+    ctx.rotate(bird.rotate * Math.PI / 180);
+
+    if (img.bird.complete) {
+        ctx.drawImage(
+        img.bird,
+        -bird.width/2,
+        -bird.height/2,
+        bird.width,
+        bird.height
+    );
+} else {
+    ctx.fillStyle = '#FFD700';
+    ctx.fillRect(-bird.width/2, -bird.height/2, bird.width, bird.height);
+   }
+
+   ctx.restore();
+
 }
 
 function startGame() {
@@ -60,5 +107,6 @@ function renderizar() {
 
 function desenharTelaInicial(){
     bgLoop();
+    drawBird();
     requestAnimationFrame(desenharTelaInicial);
 }
